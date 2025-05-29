bfc74f2fe8c056f4db95017af9c757b4fdd5227c
Luan Santos <github@luan.sh>
2025-05-29 14:58:34 -0700

    feat: dark mode

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import LogoutLink from './LogoutLink.svelte';
 
-	import { ChevronDownIcon, SettingsIcon, UserIcon } from '@lucide/svelte';
+	import { ChevronDownIcon, MoonIcon, SettingsIcon, SunIcon, UserIcon } from '@lucide/svelte';
+	import { resetMode, setMode } from 'mode-watcher';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 
 	import Button, { buttonVariants } from '$lib/components/ui/button/button.svelte';
@@ -14,6 +15,23 @@
 		<div class="flex h-(--header-height) w-full items-center gap-2 px-4 py-2">
 			<Button variant="ghost" href="/">Home</Button>
 			<div class="w-full"></div>
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger class={buttonVariants({ variant: 'ghost', size: 'icon' })}>
+					<SunIcon
+						class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
+					/>
+					<MoonIcon
+						class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content align="end">
+					<DropdownMenu.Item onclick={() => setMode('light')}>Light</DropdownMenu.Item>
+					<DropdownMenu.Item onclick={() => setMode('dark')}>Dark</DropdownMenu.Item>
+					<DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+
 			<DropdownMenu.Root>
 				<DropdownMenu.Trigger class={buttonVariants({ variant: 'ghost' })}
 					><UserIcon />{data.user.username}
